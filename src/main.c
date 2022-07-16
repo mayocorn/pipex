e706ce3f8177134d9074ce46c06f1921ebd210cb
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-17 06:10:40 +0900

    feat(main.c): add new file

---
+++ src/main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 23:55:55 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/07/17 04:40:13 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, const char **argv)
+{
+	if (argc != 5)
+	{
+		print_usage();
+		exit(EXIT_FAILURE);
+	}
+	return (pipex(argv));
+}
+
+static int	pipex(char **argv)
+{
+	int		pipefd[2];
+	pid_t	pid;
+	int		status;
+	pid_t	last_pid;
+	int		last_status;
+
+	wrapper_pipe(pipefd);
+	pid = wrapper_fork();
+	if (pid == 0)
+		process_child_1(argv, pipefd);
+	close(pipefd[1]);
+	pid = wrapper_fork();
+	if (pid == 0)
+		process_child_2(argv, pipefd);
+	close(pipefd[0]);
+	last_pid = pid;
+	last_status = EXIT_FAILURE;
+	pid = wait(&status);
+	while (pid != -1)
+	{
+		if (pid == last_pid)
+			last_status = status;
+		pid = wait(&status);
+	}
+	return (WIFEXITED(last_status) && WEXITSTATUS(last_status));
+}
