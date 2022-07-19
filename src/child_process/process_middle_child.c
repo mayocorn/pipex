58ced34bfd5715822ed8d2ae66a689a79c94439e
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-20 06:43:25 +0900

    feat(child_process): 間のコマンドを処理する関数を追加

--- src/child_process/process_middle_child.c
+++ src/child_process/process_middle_child.c
@@ -1,22 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   child_process.h                                    :+:      :+:    :+:   */
+/*   process_middle_child.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/19 01:56:34 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/20 06:41:46 by mayocorn         ###   ########.fr       */
+/*   Created: 2022/07/20 06:28:56 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/07/20 06:37:40 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHILD_PROCESS_H
-# define CHILD_PROCESS_H
-# include <unistd.h>
-# include "utils.h"
-# include "wrapper.h"
+#include "child_process.h"
 
-void	process_first_child(const char **argv, int pipefd[2]);
-void	process_last_child(const char **argv, int pipefd[2]);
-void	process_middle_child(const char *cmd, int pre_pipe[2], int next_pipe[2]);
-#endif
+void	process_middle_child(const char *cmd, int pre_pipe[2], int next_pipe[2])
+{
+	char	*path;
+
+	close(next_pipe[0]);
+	wrapper_dup2(pre_pipe[0], STDIN_FILENO);
+	close(pre_pipe[0]);
+	wrapper_dup2(next_pipe[1], STDOUT_FILENO);
+	close(next_pipe[1]);
+	path = search_path(cmd);
+	wrapper_execve(path, cmd);
+}
