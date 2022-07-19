88aba636fe24ebd405f7cd478a51db438656af1d
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-20 05:36:28 +0900

    feat(pipex_bonus.c): 多段パイプに対応したpipex関数を作成

---
+++ src/pipex/pipex_bonus.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 03:37:47 by mayocorn          #+#    #+#             */
+/*   Updated: 2022/07/20 05:34:33 by mayocorn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static void		create_first_child(const char **argv, int next_pipe[2]);
+static void		create_middle_child(const char *cmds, int pre_pipe[2], int next_pipe[2]);
+static pid_t	create_last_child(const char **argv, int pre_pipe[2]);
+
+static int	pipex_bonus(const int argc, const char **argv)
+{
+	int		pre_pipe[2];
+	int		next_pipe[2];
+	size_t	cmd_cnt;
+	size_t	i;
+	pid_t	last_pid;
+
+	if (argc < 5)
+		exit_print_usage_bonus();
+	cmd_cnt = argc - 3;
+	i = 0;
+	last_pid = 0;
+	while(i < cmd_cnt)
+	{
+		if (i == 0)
+			create_first_child(argv + 1, next_pipe);
+		else if (i == cmd_cnt - 1)
+			last_pid = create_last_child(argv + argc - 2, pre_pipe);
+		else
+			create_middle_child(argv[i + 2], pre_pipe, next_pipe);
+		swap_pipe(pre_pipe, next_pipe);
+		i++;
+	}
+	return (wait_child_process(last_pid));
+}
+
+static void	create_first_child(const char **argv, int next_pipe[2])
+{
+	pid_t	pid;
+
+	wrapper_pipe(next_pipe);
+	pid = wrapeer_fork();
+	if (pid == 0)
+		process_first_child(argv, next_pipe);
+	close(next_pipe[1]);
+}
+
+static void	create_middle_child(const char *cmds, int pre_pipe[2], int next_pipe[2])
+{
+	pid_t	pid;
+
+	wrapper_pipe(next_pipe);
+	pid = wrapeer_fork();
+	if (pid == 0)
+		process_middle_child(cmds, pre_pipe, next_pipe);
+	close(pre_pipe[0]);
+	close(next_pipe[1]);
+}
+
+static pid_t	create_last_child(const char **argv, int pre_pipe[2])
+{
+	pid_t	pid;
+
+	pid = wrapper_fork();
+	if (pid == 0)
+		process_last_child(argv, pre_pipe);
+	close(pre_pipe[0]);
+	return (pid);
+}
