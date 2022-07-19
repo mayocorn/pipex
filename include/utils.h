5198ec806542420f5d6466864e6974de5ade73b1
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-20 06:50:19 +0900

    feat(utils): swap_pipe.cを追加・exit系の関数を集約

--- include/utils.h
+++ include/utils.h
@@ -6,21 +6,28 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 01:20:59 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/19 04:40:09 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/20 06:46:42 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
+# include <sys/types.h>
+# include <sys/wait.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft.h"
 # include "wrapper.h"
 
-void	print_usage(void);
+void	exit_print_usage(void);
+void	exit_print_usage_bonus(void);
+void	exit_print_usage_heredoc(void);
 void	exit_perror(const char *message);
 void	exit_command_not_found(char *cmd_name);
 char	*search_path(const char *cmd);
+void	swap_pipe(int pre_pipe[2], int next_pipe[2]);
 char	*find_cmd_fullpath(char *pathenv, char *cmd_name);
+void	swap_pipe(int pre_pipe[2], int next_pipe[2]);
+int		wait_child_process(pid_t last_pid);
 #endif
--- src/utils/ft_exit.c
+++ src/utils/ft_exit.c
@@ -6,11 +6,34 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 01:18:41 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/19 06:22:32 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/20 06:48:54 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
+
+void	exit_print_usage(void)
+{
+	ft_putstr_fd("pipex: invalid arguments\n", STDERR_FILENO);
+	ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_print_usage_bonus(void)
+{
+	ft_putstr_fd("pipex: invalid arguments\n", STDERR_FILENO);
+	ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 cmd3 ... cmdn file2\n", \
+					STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_print_usage_heredoc(void)
+{
+	ft_putstr_fd("pipex: invalid arguments\n", STDERR_FILENO);
+	ft_putstr_fd("Usage: ./pipex here_doc LIMITTER cmd cmd file\n", \
+					STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
 
 void	exit_perror(const char *message)
 {
