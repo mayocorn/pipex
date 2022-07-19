cb850b3c47540ada6ef8ec9de0d0798dad446c1f
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-20 05:17:50 +0900

    feat(print_usage): ボーナス部分を追加

--- src/utils/print_usage.c
+++ src/utils/print_usage.c
@@ -6,14 +6,29 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:48:42 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/19 02:50:14 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/20 01:18:24 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-void	print_usage(void)
+void	exit_print_usage(void)
 {
 	ft_putstr_fd("pipex: invalid arguments\n", STDERR_FILENO);
 	ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_print_usage_bonus(void)
+{
+	ft_putstr_fd("pipex: invalid arguments\n", STDERR_FILENO);
+	ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 cmd3 ... cmdn file2\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_print_usage_heredoc(void)
+{
+	ft_putstr_fd("pipex: invalid arguments\n", STDERR_FILENO);
+	ft_putstr_fd("Usage: ./pipex here_doc LIMITTER cmd cmd file\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
 }
