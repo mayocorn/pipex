f0a57888c59b9eb6c45b89639ec7a7420da7b570
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-21 06:36:23 +0900

    feat(process_last_child): 作成するファイルのflagを受け取るように変更

--- src/child_process/process_last_child.c
+++ src/child_process/process_last_child.c
@@ -6,26 +6,25 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 02:39:20 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/20 06:17:15 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/21 05:17:11 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "child_process.h"
 
-void	process_last_child(const char **argv, int pipefd[2])
+void	process_last_child(const char **argv, int pre_fd[2], int oflag)
 {
 	const char	*outfile_name;
 	const char	*cmd;
 	char		*path;
 	int			outfile_fd;
 
-	wrapper_dup2(pipefd[0], STDIN_FILENO);
-	close(pipefd[0]);
-	outfile_name = argv[1];
-	cmd = argv[0];
+	wrapper_dup2(pre_fd[0], STDIN_FILENO);
+	close(pre_fd[0]);
 	path = search_path(cmd);
-	outfile_fd = wrapper_open_mode(outfile_name, \
-									O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	outfile_name = argv[1];
+	outfile_fd = wrapper_open_mode(outfile_name, oflag, 0666);
+	cmd = argv[0];
 	wrapper_dup2(outfile_fd, STDOUT_FILENO);
 	close(outfile_fd);
 	wrapper_execve(path, cmd);
