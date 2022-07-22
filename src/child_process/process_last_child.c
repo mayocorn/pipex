76abcb1d28125697af2a8a3b724dc9a2736144d2
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-22 09:47:05 +0900

    fix(process_last_child): コマンドが見つからないときもファイルを作成するように変更

--- src/child_process/process_last_child.c
+++ src/child_process/process_last_child.c
@@ -6,7 +6,7 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 02:39:20 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/22 07:26:51 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/22 09:46:19 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	process_last_child(const char *cmd, const char *outfile, \
 
 	wrapper_dup2(pre_pipe[0], STDIN_FILENO);
 	close(pre_pipe[0]);
-	path = search_path(cmd);
 	fd = wrapper_open_mode(outfile, oflag, 0666);
+	path = search_path(cmd);
 	wrapper_dup2(fd, STDOUT_FILENO);
 	close(fd);
 	wrapper_execve(path, cmd);
