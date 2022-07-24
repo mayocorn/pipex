f629855d1e886ec2304d8a3a486d2993a55b277b
mayocorn <stsunoda@student.42tokyo.jp>
2022-07-24 13:36:44 +0900

    refactor(read_heredoc): 関数を分割

--- src/utils/read_heredoc.c
+++ src/utils/read_heredoc.c
@@ -6,35 +6,39 @@
 /*   By: mayocorn <twitter@mayocornsuki>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 01:36:47 by mayocorn          #+#    #+#             */
-/*   Updated: 2022/07/24 02:53:20 by mayocorn         ###   ########.fr       */
+/*   Updated: 2022/07/24 13:34:45 by mayocorn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
+static char	*create_heredoc_str(const char *limiter);
+
 char	*read_heredoc(const char **argv)
 {
-	const char	*limiter;
-	size_t		limiter_len;
-	char		*res;
-	char		*pre;
-	char		*str;
-
-	if (ft_strncmp(argv[1], "here_doc", 8) != 0)
+	if (ft_strcmp(argv[1], "here_doc") != 0)
 		return (NULL);
-	limiter = argv[2];
-	res = wrapper_calloc(1, sizeof(char));
+	return (create_heredoc_str(argv[2]));
+}
+
+static char	*create_heredoc_str(const char *limiter)
+{
+	size_t	limiter_len;
+	char	*res;
+	char	*pre_res;
+	char	*str;
+
 	limiter_len = ft_strlen(limiter);
 	str = get_next_line(STDIN_FILENO);
 	while (str != NULL)
 	{
 		if (ft_strlen(str) == limiter_len + 1 && \
 				ft_strncmp(str, limiter, limiter_len) == 0)
-			break;
-		pre = res;
+			break ;
+		pre_res = res;
 		res = wrapper_strjoin(res, str);
+		free(pre_res);
 		free(str);
-		free(pre);
 		str = get_next_line(STDIN_FILENO);
 	}
 	free(str);
